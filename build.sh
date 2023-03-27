9785e950a41451329f0e6c02fb73ab3d00613b1b
Giorgi Tevdoradze <tevdoradzege@gmail.com>
2023-03-27 13:52:48 +0200

    Adds build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,18 @@
 
 set -ueo pipefail
 
-echo "TEST"
+ls *md | sed 's/\.md//g' | awk '{ print "["$0"]("$0".html)" }' >index.md
+rm -r .output
+
+REMOTE="$(git remote -v | head -n1 | cut -f2 | cut -d ' ' -f 1)"
+git clone --branch gh-pages $REMOTE .output
+
+for MD in *md;
+do (pandoc --template="template.html" $MD >".output/$( basename $MD .md ).html") done
+
+cd .output
+git add .
+git commit -m "Updates the HTML ($DATE)"
+git push origin gh-pages
+
+cd ../
